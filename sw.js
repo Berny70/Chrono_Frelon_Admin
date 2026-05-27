e38346f6c33aa287c853f773e67ea9536dff2c52
Bernard BARROIS <37933006+Berny70@users.noreply.github.com>
2026-05-27 14:27:25 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-// Service Worker désactivé intentionnellement — ChassNid v1.0
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
