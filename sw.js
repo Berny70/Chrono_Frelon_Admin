de98ecfea194507b01056565194f59e40f07217b
Bernard BARROIS <37933006+Berny70@users.noreply.github.com>
2026-07-11 10:10:20 +0200

    chore: bump 1.3.4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// Service Worker ChassNid v1.3.3
-const CACHE_VERSION = 'chassnid-v1.3.3';
+// Service Worker ChassNid v1.3.4
+const CACHE_VERSION = 'chassnid-v1.3.4';
 
 self.addEventListener('install', () => self.skipWaiting());
 
